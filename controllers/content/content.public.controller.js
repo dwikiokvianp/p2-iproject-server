66a3b059fc061b5c101ecb6e120f2fb72565951d
dwikiokvianp <dwikiokvianpradana@gmail.com>
2023-02-08 20:44:02 +0700

    feat : add summarize paragraph feature

--- controllers/content/content.public.controller.js
+++ controllers/content/content.public.controller.js
@@ -1,5 +1,7 @@
 const { Content, Topic, User } = require("../../models");
 
+const axios = require("axios").default;
+
 class ContentPublicController {
   static async getContentById(req, res, next) {
     try {
@@ -8,6 +10,16 @@ class ContentPublicController {
         where: {
           id,
         },
+        include: [
+          {
+            model: Topic,
+            attributes: ["name"],
+          },
+          {
+            model: User,
+            attributes: ["name", "job"],
+          },
+        ],
       });
       if (!content) throw { name: "ContentNotFound" };
       res.status(200).json(content);
@@ -36,6 +48,35 @@ class ContentPublicController {
       res.status(200).json(contents);
     } catch (err) {
       next(err);
+    }
+  }
+
+  static async summarizeParagraph(req, res, next) {
+    try {
+      const { id } = req.params;
+      const content = await Content.findOne({
+        where: {
+          id,
+        },
+      });
+      if (!content) throw { name: "ContentNotFound" };
+      const { contentFill } = content;
+      const options = {
+        method: "POST",
+        url: "https://tldrthis.p.rapidapi.com/v1/model/abstractive/summarize-text/",
+        headers: {
+          "content-type": "application/json",
+          "X-RapidAPI-Key":
+            "b3e9d28835msh33d0c98aafe7148p155487jsn613f84175c84",
+          "X-RapidAPI-Host": "tldrthis.p.rapidapi.com",
+        },
+        data: `{"text": ${JSON.stringify(contentFill)}}`,
+      };
+
+      const response = await axios.request(options);
+      res.status(200).json(response.data);
+    } catch (err) {
+      res.send(err);
     }
   }
 }
--- routes/content/content.public.routes.js
+++ routes/content/content.public.routes.js
@@ -3,6 +3,10 @@ const contentPublicRoutes = require("express").Router();
 const ContentController = require("../../controllers/content/content.public.controller");
 
 contentPublicRoutes.get("/", ContentController.getAllContent);
+contentPublicRoutes.post(
+  "/summarize/:id",
+  ContentController.summarizeParagraph
+);
 contentPublicRoutes.get("/:id", ContentController.getContentById);
 
 module.exports = contentPublicRoutes;
