ff4799395a3fde446eb30326cbd676fcb40121b0
dwikiokvianp <dwikiokvianpradana@gmail.com>
2023-02-08 17:37:13 +0700

    feat : add get content by id

--- controllers/content/content.public.controller.js
+++ controllers/content/content.public.controller.js
@@ -1,16 +1,34 @@
 const { Content, Topic, User } = require("../../models");
 
 class ContentPublicController {
+  static async getContentById(req, res, next) {
+    try {
+      const { id } = req.params;
+      const content = await Content.findOne({
+        where: {
+          id,
+        },
+      });
+      if (!content) throw { name: "ContentNotFound" };
+      res.status(200).json(content);
+    } catch (err) {
+      next(err);
+    }
+  }
+
   static async getAllContent(req, res, next) {
     try {
       const contents = await Content.findAll({
-        include: [{
-          model: Topic, 
-          attributes: ["name"],
-        },{
-          model: User,
-          attributes: ["name", "job"],
-        }],
+        include: [
+          {
+            model: Topic,
+            attributes: ["name"],
+          },
+          {
+            model: User,
+            attributes: ["name", "job"],
+          },
+        ],
         where: {
           contentType: "public",
         },
--- routes/content/content.public.routes.js
+++ routes/content/content.public.routes.js
@@ -3,5 +3,6 @@ const contentPublicRoutes = require("express").Router();
 const ContentController = require("../../controllers/content/content.public.controller");
 
 contentPublicRoutes.get("/", ContentController.getAllContent);
+contentPublicRoutes.get("/:id", ContentController.getContentById);
 
 module.exports = contentPublicRoutes;
